db6ae33de847f7456eb3121f1098cc378f0b60dd
33Evgenyy33@gmail.com <112233qqwa>
2017-08-30 19:57:44 +0300

    store locator

--- wp-content/plugins/AffiliateWP-master/includes/admin/tools/export/class-batch-export-referrals.php
+++ wp-content/plugins/AffiliateWP-master/includes/admin/tools/export/class-batch-export-referrals.php
@@ -60,7 +60,6 @@ class Export_Referrals extends Batch\Export\CSV implements Batch\With_PreFetch {
 	 */
 	public $date = array();
 
-	public $report = array();
 	/**
 	 * Status to export referrals for.
 	 *
@@ -143,15 +142,19 @@ class Export_Referrals extends Batch\Export\CSV implements Batch\With_PreFetch {
 	 */
 	public function csv_cols() {
 		return array(
-			'campaign'    => 'Партнер',
-			'email'       => __( 'Email', 'affiliate-wp' ),
-			'coupon'      => 'Промокод',
-			'description' => __( 'Description', 'affiliate-wp' ),
-			'reference'   => __( 'Reference', 'affiliate-wp' ),
-			'amount'      => __( 'Amount', 'affiliate-wp' ),
-			'date'        => __( 'Date', 'affiliate-wp' )
-			/*'billing_partner' => 'Форма оплаты',
-			'actual_address' => 'Фактический адрес'*/
+			'affiliate_id'  => __( 'Affiliate ID', 'affiliate-wp' ),
+			'email'         => __( 'Email', 'affiliate-wp' ),
+			'name'          => __( 'Name', 'affiliate-wp' ),
+			'payment_email' => __( 'Payment Email', 'affiliate-wp' ),
+			'username'      => __( 'Username', 'affiliate-wp' ),
+			'amount'        => __( 'Amount', 'affiliate-wp' ),
+			'currency'      => __( 'Currency', 'affiliate-wp' ),
+			'description'   => __( 'Description', 'affiliate-wp' ),
+			'campaign'      => __( 'Campaign', 'affiliate-wp' ),
+			'reference'     => __( 'Reference', 'affiliate-wp' ),
+			'context'       => __( 'Context', 'affiliate-wp' ),
+			'status'        => __( 'Status', 'affiliate-wp' ),
+			'date'          => __( 'Date', 'affiliate-wp' ),
 		);
 	}
 
@@ -188,6 +191,7 @@ class Export_Referrals extends Batch\Export\CSV implements Batch\With_PreFetch {
 		);
 
 		$data         = array();
+		$data_buff    = array();
 		$affiliates   = array();
 		$referral_ids = array();
 		$referrals    = affiliate_wp()->referrals->get_referrals( $args );
@@ -234,158 +238,22 @@ class Export_Referrals extends Batch\Export\CSV implements Batch\With_PreFetch {
 
 		}
 
+		/*$test111 = $this->get_stat_affil();
+		array_push( $data, $data_buff );
+		array_push( $data, $test111 );*/
+		file_put_contents( $_SERVER['DOCUMENT_ROOT'] . "/logs/aff_wp.txt", print_r( $data, true ), FILE_APPEND | LOCK_EX );
+		file_put_contents( $_SERVER['DOCUMENT_ROOT'] . "/logs/aff_wp.txt", print_r( "\n", true ), FILE_APPEND | LOCK_EX );
+		file_put_contents( $_SERVER['DOCUMENT_ROOT'] . "/logs/aff_wp.txt", print_r( "==========================================", true ), FILE_APPEND | LOCK_EX );
+		file_put_contents( $_SERVER['DOCUMENT_ROOT'] . "/logs/aff_wp.txt", print_r( "\n", true ), FILE_APPEND | LOCK_EX );
+
+
 		$data = apply_filters( 'affwp_export_get_data', $data );
 		$data = apply_filters( 'affwp_export_get_data_' . $this->export_type, $data );
 
+
+
+
 		return $data;
-	}
-
-	/*******************удаление из массива дублей по key*******************/
-	public function unique_multidim_array( $array, $key ) {
-		$temp_array = array();
-		$i          = 0;
-		$key_array  = array();
-
-		foreach ( $array as $val ) {
-			if ( ! in_array( $val[ $key ], $key_array ) ) {
-				$key_array[ $i ]  = $val[ $key ];
-				$temp_array[ $i ] = $val;
-				$i ++;
-			}
-
-		}
-
-		//unset($val);
-		return $temp_array;
-	}
-
-	public function get_stat_affil() {
-
-		$args = array(
-			'status'       => 'unpaid',
-			'date'         => ! empty( $this->date ) ? $this->date : '',
-			'affiliate_id' => $this->affiliate_id,
-			'number'       => - 1
-		);
-
-		$data_referrals = array();
-		$referrals      = affiliate_wp()->referrals->get_referrals( $args );
-
-		//print_r($referrals);
-
-		if ( $referrals ) {
-
-			foreach ( $referrals as $referral ) {
-
-				$data_referrals[] = array(
-					'affiliate_id'    => $referral->affiliate_id,
-					'campaign'        => affwp_get_affiliate_name( $referral->affiliate_id ),
-					'email'           => affwp_get_affiliate_email( $referral->affiliate_id ),
-					'reference'       => $referral->reference,
-					'amount'          => $referral->amount,
-					'payment_details' => get_userdata( affwp_get_affiliate( $referral->affiliate_id )->user_id )->billing_partner
-				);
-
-			}
-			//unset($referral);
-
-		}
-
-		asort( $data_referrals );
-
-		//return $data_referrals;
-
-		$affiliates = $this->unique_multidim_array( $data_referrals, 'affiliate_id' );
-
-		//return $affiliates;
-
-		//$report = array();
-
-		$report = array();
-
-		/*******************************
-		 * Orange - 58961
-		 * Vodafone - 58981
-		 * Ortel - 58995
-		 * EuropaSim - 59104
-		 * Globalsim - 59021
-		 * Globalsim Internet - 59004
-		 * Globalsim USA - 59135
-		 * TravelChat - 59130
-		 * Three - 59140
-		 * ******************************/
-
-		foreach ( $affiliates as $affiliate ) {
-
-			$report[] = array(
-				'affiliate_id'    => $affiliate['affiliate_id'],
-				'campaign'        => $affiliate['campaign'],
-				'email'           => $affiliate['email'],
-				'simcards_qty'    => array(
-					'58961' => 0,
-					'58981' => 0,
-					'58995' => 0,
-					'59104' => 0,
-					'59021' => 0,
-					'59004' => 0,
-					'59135' => 0,
-					'59130' => 0,
-					'59140' => 0,
-				),
-				'amount'          => 0,
-				'payment_details' => ''
-			);
-
-		}
-
-		$i = 0;
-		foreach ( $data_referrals as $data_referral ) {
-
-			$test1 = wc_get_order( $data_referral['reference'] );
-			/*if ($test1 == '') {
-				file_put_contents( $_SERVER['DOCUMENT_ROOT'] . "/logs/aff_wp.txt", print_r( 'да', true ), FILE_APPEND | LOCK_EX );
-				file_put_contents( $_SERVER['DOCUMENT_ROOT'] . "/logs/aff_wp.txt", print_r( "\n", true ), FILE_APPEND | LOCK_EX );
-			}*/
-
-			if ( $test1 == '' ) {
-				continue;
-			}
-
-			if ( $affiliates[ $i ]['affiliate_id'] != $data_referral['affiliate_id'] ) {
-				$i ++;
-			}
-
-			/* echo '$affiliates = ' . $affiliates[$i]['affiliate_id'] . '<br>';
-			 echo '$data_referral = ' . $data_referral['affiliate_id'] . '<br>';
-			 echo 'order: ' . $data_referral['reference'] . '<br><br>';*/
-
-			$report[ $i ]['amount']          += $data_referral['amount'];
-			$report[ $i ]['payment_details'] = $data_referral['payment_details'];
-
-			$order = new WC_Order( $data_referral['reference'] );
-			$items = $order->get_items();
-
-			foreach ( $items as $key => $item ) {
-
-				if ( get_post_meta( $item['product_id'], '_affwp_' . 'woocommerce' . '_referrals_disabled', true ) ) {
-					continue; // Referrals are disabled on this product
-				}
-
-				//Пропустить симки в отчете
-				//if  ($item['product_id'] == 41120 || $item['product_id'] == 48067) continue; //travelchat
-
-				/*if ($data_referral['affiliate_id'] == 16)
-					echo 'товар: ' . $item['product_id'] . ' кол-во: ' . $item['qty'] . '<br>';*/
-
-				$report[ $i ]['simcards_qty'][ $item['product_id'] ] += $item['qty'];
-
-			}
-
-			unset( $order );
-		}
-
-		return $report;
-
 	}
 
 	/**
